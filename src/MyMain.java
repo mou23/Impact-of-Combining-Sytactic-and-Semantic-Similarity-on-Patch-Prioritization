2bae2e48862dc56853f65c72e07e0a7775534bc9
Moumita Asad <bsse0731@iit.du.ac.bd>
2019-10-10 19:31:34 +0600

    removed duplicates from other projects

--- src/MyMain.java
+++ src/MyMain.java
@@ -16,9 +16,8 @@ import org.apache.commons.io.FilenameUtils;
 import net.lingala.zip4j.core.ZipFile;
 
 public class MyMain {
-	static String dirLocation = "dataset/buggy0/";
+	static String dirLocation = "dataset/buggy/";
 	static File rootFolder = new File(dirLocation);
-
 	public static void main(String[] args) {
 		
 		File[] listOfFiles = rootFolder.listFiles();
@@ -26,35 +25,34 @@ public class MyMain {
 		try {
 			for (int i = 0; i < listOfFiles.length; i++) {
 				scanDirectory(listOfFiles[i]);
-				break;
+//				break;
 			}
-
 		} catch(Exception e) {
 			e.printStackTrace();
 			System.out.println(e.getLocalizedMessage());
 		}
 	}
 
-	private static void scanDirectory(File folder) {
+	private static void scanDirectory(File folder) throws Exception {
 		File[] listOfFiles = folder.listFiles();
 
 		for (int i = 0; i < listOfFiles.length; i++) {
 			if (listOfFiles[i].isFile() && listOfFiles[i].getName().contains(".java")) {
 				String file = listOfFiles[i].getAbsolutePath();
 				file = file.substring(file.indexOf(rootFolder.getName())+rootFolder.getName().length()+1);
-				System.out.println("Processing " +file);
+//				System.out.println("Processing " +file);
 				long startingTime = System.nanoTime();
 //				FaultLocalizer faultLocalizer = FaultLocalizer.createFaultLocalizer();
 //				faultLocalizer.localizeFault(file);
 //				PatchEvaluator patchEvaluator = PatchEvaluator.createPatchEvaluator();
 //				patchEvaluator.prepareSolutionAST(file);
-				PatchGenerator patchGenerator = PatchGenerator.createPatchGenerator(); 
-				String identifier = listOfFiles[i].getParent();
-				identifier = identifier.substring(identifier.indexOf(rootFolder.getName())+rootFolder.getName().length()+1);
+//				PatchGenerator patchGenerator = PatchGenerator.createPatchGenerator(); 
+//				String identifier = listOfFiles[i].getParent();
+//				identifier = identifier.substring(identifier.indexOf(rootFolder.getName())+rootFolder.getName().length()+1);
 //				System.out.println(identifier);
-				patchGenerator.generatePatch(listOfFiles[i], identifier, startingTime);
-				System.out.println();
-				System.out.println();
+//				patchGenerator.generatePatch(listOfFiles[i], identifier, startingTime);
+//				System.out.println();
+//				System.out.println();
 				break;
 			}
 			else if (listOfFiles[i].isDirectory()) {
